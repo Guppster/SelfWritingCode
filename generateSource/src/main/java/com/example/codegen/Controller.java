7078c8eafd6ea7bcffd0ff38054c155ed0e0c26d
Gurpreet Singh <gsingh@ca.ibm.com>
2017-04-12 14:59:10 -0400

    Tryed out typesafe class for each objeect in config

---
+++ generateSource/src/main/java/com/example/codegen/Controller.java
@@ -0,0 +1,52 @@
+package com.example.codegen;
+
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+import com.typesafe.config.ConfigRenderOptions;
+
+import java.io.File;
+
+public class Controller
+{
+    public static void main(String[] args)
+    {
+        typeSafeConfTesting();
+
+        //Get the variables from configuration
+
+        //Populate the template with variables
+
+        //Run the program
+    }
+
+    private static void typeSafeConfTesting()
+    {
+        File configFile = new File("/home/gsingh/Documents/Projects/config.conf");
+
+        // usual Typesafe Config mechanism to load the file
+        Config tsConfig = ConfigFactory.parseFile(configFile).resolve();
+
+        // create instance of the type class. This will
+        // perform all validations according to required properties and types:
+        Properties config = new Properties(tsConfig);
+
+        int age = config.Person.age;
+        String name = config.Person.name;
+        String email = config.Person.email;
+        String location = config.Person.location;
+
+        System.out.println("location = " + location);
+        System.out.println("email = " + email);
+        System.out.println("name = " + name);
+        System.out.println("age = " + age);
+
+        System.out.println("\n*** Typesafe rendering of input Config object: *** ");
+        ConfigRenderOptions options = ConfigRenderOptions.defaults()
+                .setFormatted(true)
+                .setComments(true)
+                .setOriginComments(false);
+
+        System.out.println(tsConfig.root().render(options));
+    }
+
+}
